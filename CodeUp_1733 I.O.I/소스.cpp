c2625b015a76a9ec26c239191140ff0db339e7ee
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-05-14 14:20:45 +0900

    초기 커밋.

---
+++ CodeUp_1733 I.O.I/소스.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	if (str == "IOI")
+		cout << "IOI is the International Olympiad in Informatics.";
+	else
+		cout << "I don't care.";
+	return 0;
+}
